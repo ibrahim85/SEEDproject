6179986f1c3af956061c0ab0d708f71011b1f877
yujiex <shenqing1225@gmail.com>
2015-11-16 17:44:23 -0500

    added field renaming

--- Code/converter/converter.py
+++ Code/converter/converter.py
@@ -10,8 +10,19 @@ write_file_name = "test.txt"
 
 meter_con_df = pd.read_excel(read_dir_name + read_file_name, sheetname='Meter Consumption Data', skiprows=4, header = 5)
 
+# Calculate time interval of days
+meter_con_df['interval'] = meter_con_df['End Date'] - meter_con_df['Start Date']
+meter_con_df['reading_kind'] = 'energy'
+
+# renaming columns of df
+name_lookup = {u'Start Date':u'start',
+               u'End Date':u'End Date',
+               u'Usage/Quantity':u'value',
+               u'Usage Units':'uom'}
+
+meter_con_df = meter_con_df.rename(columns=name_lookup)
+
+# the 'interval' output is in [ns]
 d = meter_con_df.to_json(write_dir_name + write_file_name, 'records', date_unit = 's')
-#json.dump(d, open(write_dir_name + write_file_name, 'w'))
 
 # TO DOs
-# df header name conversion from PM to postgreSQL
