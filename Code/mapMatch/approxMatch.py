2e2cc8e59bfb20cc6ded3d3aa96f486bda56fbf7
yujiex <shenqing1225@gmail.com>
2015-12-01 13:49:27 -0500

    finished matching function with test cases from selected points, finished document

--- Code/mapMatch/approxMatch.py
+++ Code/mapMatch/approxMatch.py
@@ -1,3 +1,15 @@
+## ## ## ## ## ## ## ## ## ## ##
+## ## Usage ## ##
+## ## ## ## ## ## ## ## ## ## ##
+##
+## Run approxMatch with '-v' for verbose mode: python -v approxMatch
+## Run approxMatch with '-d' for debug mode:python -d approxMatch
+##
+## Supported options:
+##   -v, --verbose  show info
+##   -d, --debug    debug printing
+## ## ## ## ## ## ## ## ## ## ##
+
 import os
 import pandas as pd
 from fuzzywuzzy import fuzz
@@ -5,6 +17,9 @@ from fuzzywuzzy import fuzz
 ## ## ## ## ## ## ## ## ## ## ##
 ## ## utilities ## ##
 ## ## ## ## ## ## ## ## ## ## ##
+
+## ## ## ## ## ## ## ## ## ## ##
+## logging and debugging print settings
 import getopt
 import logging
 import sys
@@ -30,7 +45,7 @@ def printDict(d, limit):
     iterator = iter(d)
     while count < limit:
         key = next(iterator)
-        print (key, d[key])
+        print '{0} -> {1}'.format(key, d[key])
         count += 1
 
 ## ## ## ## ## ## ## ## ## ## ##
@@ -60,6 +75,9 @@ add_abbrev_dict = readAddressLookup()
 
 def preProcess(s, reason):
     if reason == 'address':
+        # remove the trailing dot
+        if s[len(s) - 1] == '.':
+            s = s[:-1]
         L = s.split()
         logger.debug('tokenize: ')
         logger.debug('input: {0}, output: {1}'.format(s, L))
@@ -68,11 +86,23 @@ def preProcess(s, reason):
             try:
                 acc.append(add_abbrev_dict[word.upper()])
             except KeyError:
-                #print '{0} not an address abbreviation'.format(word.upper())
+                logger.debug('{0} not an address abbreviation'.format(word.upper()))
                 acc.append(word)
         return ' '.join(acc)
     else:
         return s
+
+def test_preProcess():
+    print('\ntest_preProcess')
+    dirname = os.getcwd() + '/testInput/'
+    # source of input: randomly picked points from Google Maps
+    filename = 'address.csv'
+    df = pd.read_csv(dirname + filename)
+    print("read randomly picked address:")
+    print(df['input'])
+    df['output'] = df['input'].map(lambda x: preProcess(x, 'address'))
+    print(df)
+    return
 
 # approximate string similarity evaluation with fuzzywuzzy package
 def similarity(s, t, method):
@@ -86,7 +116,7 @@ def similarity(s, t, method):
         return fuzz.ratio(s, t)
 
 def test_similarity(s, t):
-    print('\ntest_similarity')
+    print('\ntest_similarity({0}, {1})').format(s, t)
     s1 = preProcess(s, 'address')
     print 'original: {0}, after preProcess: {1}'.format(s, s1)
     methods = ['simple', 'partial', 'token_sort', 'token_set']
@@ -96,9 +126,12 @@ def test_similarity(s, t):
     return
 
 def main():
-    test_readAddressLookup(5)
     s = '5000 Forbes Avenue'
     t = '5000 Forbes Ave'
     test_similarity(s, t)
+    '''
+    test_readAddressLookup(30)
+    test_preProcess()
+    '''
 
 main()
